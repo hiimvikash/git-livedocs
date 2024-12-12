2c0de6ff20408b75f31bb2da9156ea6d6b58337f
hiimvikash <heyimvikash@gmail.com>
2024-12-12 10:55:55 +0530

    01-nextjs+shadcn-set

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Hello World</p>
+      <Button variant="test">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
